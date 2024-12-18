cf6985f0612dfc676d7a3998db2a5c06db1d967e
dmihaescu <danmihaescu19@gmail.com>
2024-12-18 11:32:13 +0200

    Tema4 upload

--- inc/book.hpp
+++ inc/book.hpp
@@ -3,28 +3,29 @@
 
 #include <string>
 #include <iostream>
+#include <memory> // #14
 
 class Book {
 private:
     std::string title;
-    std::string* author;
+    std::shared_ptr<std::string> author; // #13
     int year;
 
 public:
     Book(const std::string& title = "Unknown Title",
          const std::string& author = "Unknown Author",
-         int year = 0); 
+         int year = 0);
 
     ~Book();
 
-    Book(const Book& other); 
+    Book(const Book& other);
     Book(Book&& other) noexcept;
 
-    Book& operator=(const Book& other); 
-    Book& operator=(Book&& other) noexcept; 
+    Book& operator=(const Book& other);
+    Book& operator=(Book&& other) noexcept;
 
-    Book& operator=(int) = delete; 
-    Book(int) = delete; 
+    Book& operator=(int) = delete;
+    Book(int) = delete;
 
     void display() const;
 };
--- src/book.cpp
+++ src/book.cpp
@@ -2,52 +2,47 @@
 
 Book::Book(const std::string& title, const std::string& author, int year) {
     this->title = title;
-    this->author = new std::string(author);
+    this->author = std::make_shared<std::string>(author); // #13
     this->year = year;
-    std::cout << "Book constructor called for: " << this->title << std::endl; 
+    std::cout << "Book constructor called for: " << this->title << std::endl;
 }
 
 Book::~Book() {
-    delete author;
     std::cout << "Book destructor called for: " << this->title << std::endl;
 }
 
 Book::Book(const Book& other) {
     this->title = other.title;
-    this->author = new std::string(*other.author);
+    this->author = other.author; // #14
     this->year = other.year;
-    std::cout << "Book copy constructor called for: " << this->title << std::endl; 
+    std::cout << "Book copy constructor called for: " << this->title << std::endl;
 }
 
 Book::Book(Book&& other) noexcept {
     this->title = std::move(other.title);
-    this->author = other.author;
+    this->author = std::move(other.author);
     this->year = other.year;
-    other.author = nullptr;
     std::cout << "Book move constructor called for: " << this->title << std::endl;
 }
 
 Book& Book::operator=(const Book& other) {
-    if (this != &other) { 
-        delete this->author;
+    if (this != &other) {
         this->title = other.title;
-        this->author = new std::string(*other.author); 
+        this->author = other.author; // #14
         this->year = other.year;
     }
     std::cout << "Book copy assignment operator called for: " << this->title << std::endl;
-    return *this; 
+    return *this;
 }
 
 Book& Book::operator=(Book&& other) noexcept {
-    if (this != &other) { 
-        delete this->author;
+    if (this != &other) {
         this->title = std::move(other.title);
-        this->author = other.author;
+        this->author = std::move(other.author);
         this->year = other.year;
-        other.author = nullptr;
     }
     std::cout << "Book move assignment operator called for: " << this->title << std::endl;
-    return *this; 
+    return *this;
 }
 
 void Book::display() const {
